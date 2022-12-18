4f65d4dd9126f85a871d08051e27276d5a29612d
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-18 08:50:18 +0530

    updated router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Home  from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/signup" element={<SignUp/>} />
+            <Route exact path="/login" element={<Login />} />
+      </Routes>
+
+    </Router>
+    
   );
 }
 
