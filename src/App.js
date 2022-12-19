debaf6f396c9f4523507075d01dcb339319a707d
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-19 17:13:31 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import SignUp from './components/SignUp';
 import Home  from './components/Home';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import News from './components/News';
 function App() {
   return (
     <Router>
       <Header/>
       <Routes>
-            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/" element={<Home Component={News}/>} />
+            <Route exact path="/news" element={<News/>} />
             <Route exact path="/signup" element={<SignUp/>} />
             <Route exact path="/login" element={<Login />} />
       </Routes>
