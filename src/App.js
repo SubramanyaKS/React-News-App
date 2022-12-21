33cab8bc51f82266c1b0d37b77f12f68ad7e9787
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-21 22:34:34 +0530

    updated sports

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Home  from './components/Home';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import News from './components/News';
+import Sports from './components/Sports';
 function App() {
   return (
     <Router>
       <Header/>
       <Routes>
             <Route exact path="/" element={<Home Component={News}/>} />
+            <Route exact path="/sports" element={<Home Component={Sports}/>} />
             <Route exact path="/news" element={<News/>} />
             <Route exact path="/signup" element={<SignUp/>} />
             <Route exact path="/login" element={<Login />} />
