04d2b9c4b690573926aca63eebbfce0634db5744
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-22 19:58:24 +0530

    updated navbar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import News from './components/News';
 import Sports from './components/Sports';
+import Business from './components/Business';
 function App() {
   return (
     <Router>
@@ -15,6 +16,7 @@ function App() {
       <Routes>
             <Route exact path="/" element={<Home Component={News}/>} />
             <Route exact path="/sports" element={<Home Component={Sports}/>} />
+            <Route exact path="/business" element={<Home Component={Business}/>} />
             <Route exact path="/news" element={<News/>} />
             <Route exact path="/signup" element={<SignUp/>} />
             <Route exact path="/login" element={<Login />} />
