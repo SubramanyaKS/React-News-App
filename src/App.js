c5113ecab052893649756149375892f8979686d5
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-22 20:14:17 +0530

    updated route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import News from './components/News';
 import Sports from './components/Sports';
 import Business from './components/Business';
+import Science from './components/Science';
+import Health from './components/Health';
 function App() {
   return (
     <Router>
@@ -17,6 +19,8 @@ function App() {
             <Route exact path="/" element={<Home Component={News}/>} />
             <Route exact path="/sports" element={<Home Component={Sports}/>} />
             <Route exact path="/business" element={<Home Component={Business}/>} />
+            <Route exact path="/science" element={<Home Component={Science}/>} />
+            <Route exact path="/health" element={<Home Component={Health}/>} />
             <Route exact path="/news" element={<News/>} />
             <Route exact path="/signup" element={<SignUp/>} />
             <Route exact path="/login" element={<Login />} />
