ed71fcbbfd509f78dfe3ed8224c6c84b56e49488
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-22 19:58:44 +0530

    updated business

---
+++ src/components/Business.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Items from "./Items";
+const Business = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const news = async () => {
+      axios
+        .get(
+          `https://newsapi.org/v2/top-headlines?q=business&apiKey=${process.env.REACT_APP_API_KEY}`
+        )
+        .then((response) => {
+          setArticles(response.data.articles);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    console.log("News",news());
+  }, []);
+
+  return (
+    <div className="home">
+      <h6>Business News</h6>
+      <div>
+        {articles.map((article) => {
+          return (
+            <Items
+              title={article.title}
+              description={article.description}
+              url={article.url}
+              urlToImage={article.urlToImage}
+              publishedAt={article.publishedAt}
+              author={article.author}
+              source={article.source.name}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+  //}
+};
+export default Business;
