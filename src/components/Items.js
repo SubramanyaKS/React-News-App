f2104b27a8f5b16e57cbf34c6864408c200624ba
Subramanya K S <subramanyaks2205@gmail.com>
2023-04-30 23:01:58 +0530

    updated card

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,9 +2,14 @@ import '../App.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-
 const Items = ({ title, description, url, urlToImage,publishedAt,author,source }) => {
-    
+  
+  var localTime = new Date(publishedAt);
+  // console.log(typeof localTime);
+  // console.log(localTime);
+  let local =''+localTime;
+
+
     return (
         // <div className="news-app">
        
@@ -14,14 +19,15 @@ const Items = ({ title, description, url, urlToImage,publishedAt,author,source }
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         {author?<Card.Subtitle>Author: {author}</Card.Subtitle>:<Card.Subtitle>Author: unknown</Card.Subtitle>}
-        <Card.Text>
+        <Card.Text className="mt-3">
          {description}
         </Card.Text>
         <a href={url} target="_blank" rel="noreferrer">
-        <Button onClick={() => { window.location.href = url; } } variant="outline-dark"><b>Read More</b></Button>
+        <Button className="mt-3" onClick={() => { window.location.href = url; } } variant="outline-dark"><b>Read More</b></Button>
         </a>
       </Card.Body>
-      <Card.Footer className="card-footer">{publishedAt}</Card.Footer>
+
+      <Card.Footer className="card-footer">{local}</Card.Footer>
     </Card>
     )
 }
