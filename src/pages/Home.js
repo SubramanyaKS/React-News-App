b0dea77a0b79986ef327df5aa420fe2139efebb1
Subramanya K S <subramanyaks2205@gmail.com>
2023-05-23 23:04:46 +0530

    removed unused code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,6 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
-// import Card from "react-bootstrap/Card";
-// import Button from "react-bootstrap/Button";
-//import Items from "./Items";
-
 const Home = (props) => {
   const {Component}=props;
-  const navigate= useNavigate();
-  useEffect(()=>{
-    const login = sessionStorage.getItem("cAuthenticated");
-    console.log("Login",login);
-    // if(!login){
-    //   navigate('/login');
-    // }
-
-  })
-  return(
-    <div>
-      <Component/>
-    </div>
-  )
-  
-  
+  return <Component/>;
+ 
 };
 export default Home;
