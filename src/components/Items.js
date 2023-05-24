09e4c854d018dcebb50155df3dd3139227ee61c9
Subramanya K S <subramanyaks2205@gmail.com>
2023-05-24 07:30:50 +0530

    updated

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,35 +1,56 @@
-import '../App.css';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import "../App.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-const Items = ({ title, description, url, urlToImage,publishedAt,author,source }) => {
-  
+const Items = ({
+  title,
+  description,
+  url,
+  urlToImage,
+  publishedAt,
+  author,
+  source,
+}) => {
   let localTime = new Date(publishedAt).toLocaleTimeString();
   // console.log(typeof localTime);
   // console.log(localTime);
   let localDate = new Date(publishedAt).toLocaleDateString();
 
-
-    return (
-        // <div className="news-app">
-       
-        <Card className=" text-center">
+  return (
+    <Card className=" text-center">
       <Card.Header className="cheader">{source}</Card.Header>
-      <Card.Img className="news-img img-fluid" variant="top" src={urlToImage} alt="image from source" />
+      <Card.Img
+        className="news-img img-fluid"
+        variant="top"
+        src={urlToImage}
+        alt="image from source"
+      />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        {author?<Card.Subtitle>Author: {author}</Card.Subtitle>:<Card.Subtitle>Author: unknown</Card.Subtitle>}
-        <Card.Text className="mt-3">
-         {description}
-        </Card.Text>
+        {author ? (
+          <Card.Subtitle>Author: {author}</Card.Subtitle>
+        ) : (
+          <Card.Subtitle>Author: unknown</Card.Subtitle>
+        )}
+        <Card.Text className="mt-3">{description}</Card.Text>
         <a href={url} target="_blank" rel="noreferrer">
-        <Button className="mt-3" onClick={() => { window.location.href = url; } } variant="outline-dark"><b>Read More</b></Button>
+          <Button
+            className="mt-3"
+            onClick={() => {
+              window.location.href = url;
+            }}
+            variant="outline-dark"
+          >
+            <b>Read More</b>
+          </Button>
         </a>
       </Card.Body>
 
-      <Card.Footer className="card-footer">Published on: {localDate} at {localTime}</Card.Footer>
+      <Card.Footer className="card-footer">
+        Published on: {localDate} at {localTime}
+      </Card.Footer>
     </Card>
-    )
-}
+  );
+};
 
 export default Items;
