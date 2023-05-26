cc73ac703bfd11b7e5ff5782c9b9c03ea6d1a28a
Subramanya K S <subramanyaks2205@gmail.com>
2023-05-26 22:28:30 +0530

    updated use fetch hooks

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,19 +5,25 @@ import { NewsState } from '../context/NewsContext';
 const useFetch = (url) => {
     const {articles,setArticles} = NewsState();
     useEffect(() => {
-      const news = async () => {
+      // Function to fetch news data
+      const fetchNews = async () => {
+        // Make an HTTP GET request to the specified URL
         axios
           .get(url)
           .then((response) => {
+            // Update the articles state with the fetched data
             setArticles(response.data.articles);
           })
           .catch((error) => {
-            console.log(error);
+            // Log any errors to the console
+            console.log('Error occurred while fetching news:',error);
           });
       };
-      news();
-     // console.log("News",news());
-    }, [url]);
+      // Call the news function to fetch data when the component mounts or when the URL changes
+      fetchNews();
+     // Dependency array [url] ensures the effect runs when the URL changes
+    }, [url, setArticles]);
+     // Return the articles state for the consuming component
     return {articles};
 }
 
